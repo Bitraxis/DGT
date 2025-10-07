a9b27c697e320c824082eeae94c54f5dad74f07d
Bitraxis <hyauzane@protonmail.com>
2025-10-07 11:25:21 +0200

    sdfdsf

--- 29.9.2025/main.py
+++ 29.9.2025/main.py
@@ -1,3 +1,13 @@
-from browser import document
+from browser import document, html
+from random import choice
 
-document <= "Hello!"
+for i in range(7):
+    x = html.DIV(str(i+1), Id="div"+str(i+1), Class="cisla")
+    x.style.border = "1px solid black"
+    x.style.background = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"][i]
+    document <= x
+    
+def zmenFarbu(ev):
+    e = document["zmeneniefarby"]
+    e.style = { "color": "red" }
+document["tlacitko"].bind("click", zmenFarbu)
