4859e65649720e93bf31054b6731389b954df683
Bitraxis <hyauzane@protonmail.com>
2025-10-13 11:22:38 +0200

    w

---
+++ 13.10.2025/main.py
@@ -0,0 +1,12 @@
+from browser import document, html
+
+ulohy = [
+    {"title": "Dokončiť DÚ", "done": False, "priority": 1},
+]
+
+def list_tasks():
+    my_div = document["tasks"]
+    my_div.clear()
+    tab = html.TABLE(Class="task-table")
+    header = html.TR([html.TH("Úloha"), html.TH("Hotovo", html.TH("Priorita"))])
+    tab <= header
