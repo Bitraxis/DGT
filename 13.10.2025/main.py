249ec61e51e9596970a1220974b4eb9d43b0f2f8
Bitraxis <hyauzane@protonmail.com>
2025-10-20 11:08:30 +0200

    er

--- 13.10.2025/main.py
+++ 13.10.2025/main.py
@@ -17,12 +17,25 @@ def list_tasks():
     for t in tasks:
         row = html.TR()
         row <= html.TD(t["title"])
-        row <= html.TD("OK" if t["done"] else "TDB")
+        toggle_btn = html.BUTTON("✓" if t["done"] else "✗")
+        toggle_btn.bind("click", toggle_done)
+        row <= html.TD(toggle_btn)
+        # row <= html.TD("✓" if t["done"] else "✗")
+        # row <= html.TD("OK" if t["done"] else "TDB")
         row <= html.TD(t["priority"])
         tab <= row
     
     my_div <= tab
 
+def toggle_done(ev):
+    btn = ev.target
+    row = btn.parentElement.parentElement
+    title = row.children[0].textContent
+    for t in tasks:
+        if t["title"] == title:
+            t["done"] = not t["done"]
+            break
+    list_tasks()
     
 def add_task(ev):
     title = document["new_task"].value
